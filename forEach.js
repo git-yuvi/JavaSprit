b6def9363377a1ce9d6e1b53eb25485e2f818fda
Yuvaraj A <104821565+git-yuvi@users.noreply.github.com>
2023-08-23 18:19:00 +0530

    Create forEach.js
    forEach() function in Array

---
+++ forEach.js
@@ -0,0 +1,11 @@
+console.log("ForEach()");
+const student =[
+    { sname:'david',age:23},
+    { sname:'arjun',age:13},
+    { sname:'vijay',age:28}
+    // "ajay","vijay","raj"
+]
+console.log(student);
+student.forEach(value=>{
+    console.log(value);
+});
