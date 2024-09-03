f0d6c944b053c9a8fbbb5ab389e9f35620ed9eab
Yuvaraj A <104821565+git-yuvi@users.noreply.github.com>
2024-09-04 02:36:25 +0530

    forEach.js

--- forEach.js
+++ forEach.js
@@ -1,9 +1,11 @@
+//Foreach
 console.log("ForEach()");
 const student =[
     { sname:'david',age:23},
     { sname:'arjun',age:13},
-    { sname:'vijay',age:28}
-    // "ajay","vijay","raj"
+    { sname:'vijay',age:28},
+    { sname:'ajith',age:27}
+    // "ajay","vijay","raj","ajith"
 ]
 console.log(student);
 student.forEach(value=>{
