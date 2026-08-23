181bda00b7cfb5a36e4626fe51035bb2114e5eac
Yuvaraj A <104821565+git-yuvi@users.noreply.github.com>
2026-08-24 01:15:15 +0530

    Create PushMethod.js
    Understanding array PUSH() method

---
+++ PushMethod.js
@@ -0,0 +1,30 @@
+//Using Express
+const express = require('express');
+
+//Create an instance of express
+const app = express();
+app.use(express.json())
+
+//Sample in-memory storage for todo items
+let todos = [];
+
+//Create a new todo item
+app.post('/todos',(req, res) => {
+    const {title, description} = req.body;
+    const newTodo = {
+        id: todos.length + 1,
+        title,
+        description
+    }
+    todos.push(newTodo);
+    console.log(todos);
+    res.status(201).json(newTodo);
+
+})
+
+//Start the server
+const port = 3000;
+app.listen(port, () => {
+    console.log("Server is listening to port "+port);
+    
+})
