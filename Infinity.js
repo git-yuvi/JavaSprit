5277a9e827f89491dac837ebeae4b05b52280ba2
Yuvaraj A <104821565+git-yuvi@users.noreply.github.com>
2026-08-29 23:47:49 +0530

    Create Infinity.js
    JavaScript Global Properties

---
+++ Infinity.js
@@ -0,0 +1,22 @@
+//Infinity Property in JavaScript
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>JavaScript Global Properties</h1>
+<h2>Infinity Property</h2>
+
+<p>1.797693134862315E+308 is the limit of a JavaScript number:</p>
+
+
+<p id="demo"></p>
+
+<script>
+let x = 1.797693134862315E+308;
+let y = x * 1.001;
+
+document.getElementById("demo").innerHTML = y;
+</script>
+
+</body>
+</html>
